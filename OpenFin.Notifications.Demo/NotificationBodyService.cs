0b500c57c4c1c717c4bc6a09a0d317563019572c
John M <john.mandia@gmail.com>
2021-04-01 15:52:27 +0100

    Updated with demo project.

---
+++ OpenFin.Notifications.Demo/NotificationBodyService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OpenFin.Notifications.Demo
+{
+    public enum BodyContentType
+    {
+        PlainText,
+        Markdown
+    }
+
+    internal static class FileExtensions
+    {
+        internal const string TextFile = "txt";
+        internal const string Markdown = "md";
+    }
+
+    internal static class NotificationBodyService
+    {
+        public static string GetNotificationBodyContent(BodyContentType contentType)
+        {
+            switch (contentType)
+            {
+                case BodyContentType.PlainText:
+                    return getBodyContentByExtension(FileExtensions.TextFile);
+                case BodyContentType.Markdown:
+                    return getBodyContentByExtension(FileExtensions.Markdown);
+                default:
+                    throw new ArgumentException("unknown content type");
+            }
+        }
+
+        public static ButtonOptions[] GenerateButtons(int buttonCount, int ctaIx)
+        {
+            var options = new List<ButtonOptions>();
+
+            for (int i = 0; i < buttonCount; i++)
+            {
+                options.Add(new ButtonOptions
+                {
+                    Title = $"Button{i + 1}",
+                    IconUrl = "https://openfin.co/favicon-32x32.png",
+                    OnNotificationButtonClick = new Dictionary<string, object>
+                        {
+                            { "btn", $"button{i + 1}" }
+                        },
+
+                    IsCallToActionButton = ctaIx - 1 == i
+                });
+            }
+
+            return options.ToArray();
+        }
+
+        private static string getBodyContentByExtension(string ext)
+        {
+            return File.ReadAllText($"body.{ext}");
+        }
+    }
+}
--- test/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Placeholder. The linter expects 'src' and 'test' directories, both containing at least one source file.
- *
- * Having this file here (and referencing it in tsconfig.json) allows the linter to run from service-tooling with no
- * modifications or exemptions required within the tooling.
- */
